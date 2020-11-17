fa8b4d6a1fd8425a34f7c2c7d350183883942aef
Zaladim <48721924+Zaladim@users.noreply.github.com>
2020-11-17 14:20:20 +0100

    Creating player
    Player can move, colide with object and shot
    Shooting is done by holding space, which rotate the cannon upward. releasing shoot in the direction of the cannon

---
+++ Assets/Scenes/scripts/Player_Cannon.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class Player_Cannon : NetworkBehaviour
+{
+    [SerializeField] private GameObject projectile;
+    [SerializeField] private GameObject cannon;
+
+    [SerializeField] private float rotate = 1.0f;
+    [Client]void Update()
+    {
+        if (!hasAuthority)
+        {
+            return;
+        }
+        print(cannon.transform.eulerAngles.x);
+        if (Input.GetKey(KeyCode.Space) && cannon.transform.eulerAngles.x > 280.0f)
+        {
+            cannon.transform.Rotate(new Vector3(-rotate, 0.0f, 0.0f) * Time.deltaTime);
+            //cannon.transform.RotateAround(cannon.transform.position, Vector3.left, rotate * Time.deltaTime);
+        } else if (cannon.transform.eulerAngles.x < 340.0f)
+        {
+            cannon.transform.Rotate(new Vector3(rotate, 0.0f, 0.0f) * Time.deltaTime);
+            //cannon.transform.RotateAround(cannon.transform.position, Vector3.left, -rotate * Time.deltaTime);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            GameObject bullet = Instantiate(projectile);
+            Rigidbody rb = bullet.GetComponent<Rigidbody>();
+            bullet.transform.position = cannon.transform.position + cannon.transform.forward * 0.5f;
+            rb.velocity = cannon.transform.forward * 5.0f;
+        }
+        
+        
+    }
+}
---
+++ Assets/Scenes/scripts/Player_Move.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using Mirror;
+using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+using Vector3 = UnityEngine.Vector3;
+
+public class Player_Move : NetworkBehaviour
+{
+    [SerializeField] private GameObject avatar;
+    [SerializeField] private GameObject cannon;
+    [SerializeField] private float speed = 5.0f;
+    [SerializeField] private float rotate = 5.0f;
+    [Client]private void Update()
+    {
+        if (!hasAuthority)
+        {
+            return;
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(avatar.transform.forward * speed * Time.smoothDeltaTime, Space.World);
+        } else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(- (avatar.transform.forward * speed * Time.smoothDeltaTime), Space.World);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //transform.Rotate(-Vector3.up * rotate * Time.deltaTime);
+            transform.RotateAround(avatar.transform.position, Vector3.up, -rotate * Time.deltaTime);
+        } else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //transform.Rotate(Vector3.up * rotate * Time.deltaTime);
+            transform.RotateAround(avatar.transform.position, Vector3.up, rotate * Time.deltaTime);
+        }
+
+        cannon.transform.position = avatar.transform.position + new Vector3(0.0f, 0.4f, 0.0f);
+        cannon.transform.eulerAngles = new Vector3(cannon.transform.eulerAngles.x, avatar.transform.eulerAngles.y, cannon.transform.eulerAngles.z);
+
+    }
+}
